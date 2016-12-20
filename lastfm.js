dbce5341e84335fa38e17b74830f0f4bb3b73362
Jani Luukko <jj.luukko@gmail.com>
2016-12-21 00:52:21 +0200

    fix nowplaying issue

--- lastfm.js
+++ lastfm.js
@@ -32,8 +32,12 @@ module.exports = function (bot) {
   }
 
   function printRes(res) {
-    let track = res.track[0]
-    track.date = new Date(parseInt(track.date.uts)*1000);
+    let track = res.track[0];
+    if (track['@attr'] && track['@attr'].nowplaying === 'true') {
+      track.date = new Date()
+    } else {
+      track.date = new Date(parseInt(track.date.uts)*1000);
+    }
     if (!currentTrack || track.date > currentTrack.date) {
       currentTrack = track;
       if (!lastAnnounce || ((new Date) - lastAnnounce) < ONE_HOUR) {
