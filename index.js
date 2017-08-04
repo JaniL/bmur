c71699f0a6d0bf136e6ac4bbc803ad16f35cc80e
Jani Luukko <jani.luukko@reaktor.com>
2017-08-04 20:44:02 +0300

    start using standard

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api')
 const token = process.env.TELEGRAM_TOKEN
 const lastfm = require('./lastfm.js')
 
 if (!token) {
-  console.error("No token set")
+  console.error('No token set')
   process.exit(1)
 }
 
-var bot = new TelegramBot(token, { polling: true });
+var bot = new TelegramBot(token, { polling: true })
 
-lastfm(bot);
+lastfm(bot)
--- lastfm.js
+++ lastfm.js
@@ -1,38 +1,38 @@
-const lastfm = require("lastfm-njs");
-const fs = require('fs');
-const moment = require('moment');
-const ONE_HOUR = 60 * 60 * 1000;
+const lastfm = require('lastfm-njs')
+const fs = require('fs')
+const moment = require('moment')
+const ONE_HOUR = 60 * 60 * 1000
 
 module.exports = (bot) => {
   const lfm = new lastfm({
     apiKey: process.env.LASTFM_KEY,
     apiSecret: process.env.LASTFM_SECRET,
     username: process.env.LASTFM_USERNAME
-  });
+  })
 
-  var tracks = [];
-  var lastAnnounce = null;
-  var groups = JSON.parse(fs.readFileSync('groups.json')) || [];
+  var tracks = []
+  var lastAnnounce = null
+  var groups = JSON.parse(fs.readFileSync('groups.json')) || []
 
   bot.on('message', (msg) => {
-    if (msg.chat.type !== 'private' && !groups.includes(msg.chat.id)) groups.push(msg.chat.id);
+    if (msg.chat.type !== 'private' && !groups.includes(msg.chat.id)) groups.push(msg.chat.id)
     fs.writeFileSync('groups.json', JSON.stringify(groups))
-  });
+  })
 
-  function formatAnnounce() {
+  function formatAnnounce () {
     let message = ''
     message += '🐻 ' + tracks[0].artist['#text'] + ' - ' + tracks[0].name + ' 🐻'
     return message
   }
 
-  function formatCommand() {
+  function formatCommand () {
     let message = ''
     message += '🐻 ' + tracks[0].artist['#text'] + ' - ' + tracks[0].name + ' 🐻'
     message += '\n' + moment(tracks[0].date).fromNow()
     return message
   }
 
-  function formatRecent(t) {
+  function formatRecent (t) {
     return '- [' + t.artist['#text'] + ' - ' + t.name + '](' + t.url + ') (' + moment(t.date).fromNow() + ')'
   }
 
@@ -41,55 +41,54 @@ module.exports = (bot) => {
   })
 
   bot.onText(/\/recent/, (msg) => {
-    var tClone = tracks.slice(0).reverse();
+    var tClone = tracks.slice(0).reverse()
     bot.sendMessage(msg.chat.id, tClone.map(formatRecent).join('\n'), {
       'disable_web_page_preview': true,
       'parse_mode': 'markdown'
     })
   })
 
-  function announce() {
+  function announce () {
     for (let group of groups) {
       bot.sendMessage(group, formatAnnounce())
     }
   }
 
-  function trackHasBeenPlayedDuringTheLastHour(track) {
-    return ((new Date) - track.date) <= ONE_HOUR
+  function trackHasBeenPlayedDuringTheLastHour (track) {
+    return ((new Date()) - track.date) <= ONE_HOUR
   }
 
-  function convertDate(track) {
+  function convertDate (track) {
     if (track['@attr'] && track['@attr'].nowplaying === 'true') {
       track.date = new Date()
     } else {
-      track.date = new Date(parseInt(track.date.uts)*1000);
+      track.date = new Date(parseInt(track.date.uts) * 1000)
     }
-    return track;
+    return track
   }
 
-  function printRes(res) {
-    if (!res || !'track' in res  || res.track.length === 0) {
-      return;
+  function printRes (res) {
+    if (!res || !('track' in res) || res.track.length === 0) {
+      return
     }
     tracks = res.track.map(convertDate)
-    if (trackHasBeenPlayedDuringTheLastHour(tracks[0]) && (!lastAnnounce || ((new Date) - lastAnnounce) >= ONE_HOUR)) {
+    if (trackHasBeenPlayedDuringTheLastHour(tracks[0]) && (!lastAnnounce || ((new Date()) - lastAnnounce) >= ONE_HOUR)) {
       lastAnnounce = new Date()
       announce()
     }
   }
 
-  function printError(err) {
-    console.error(err);
+  function printError (err) {
+    console.error(err)
   }
 
-  function fetch() {
+  function fetch () {
     lfm.user_getRecentTracks({
-       user: 'matlu_klusteri',
-       limit: 10
-    }).then(printRes, printError);
+      user: 'matlu_klusteri',
+      limit: 10
+    }).then(printRes, printError)
   }
 
   fetch()
   setInterval(fetch, 5 * 1000)
-
 }
