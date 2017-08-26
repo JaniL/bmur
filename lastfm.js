652ed92570a9ae1909934bab073586bfb4a67aa4
Jani Luukko <jj.luukko@gmail.com>
2017-08-26 15:02:29 +0300

    refactor

--- lastfm.js
+++ lastfm.js
@@ -1,18 +1,17 @@
-const lastfm = require('lastfm-njs')
+const LastFm = require('lastfm-njs')
 const fs = require('fs')
 const moment = require('moment')
 const ONE_HOUR = 60 * 60 * 1000
 const FETCH_INTERVAL = 10 * 1000 // ms
 
 module.exports = (bot) => {
-  const lfm = new lastfm({
+  const lfm = new LastFm({
     apiKey: process.env.LASTFM_KEY,
     apiSecret: process.env.LASTFM_SECRET,
     username: process.env.LASTFM_USERNAME
   })
 
   var tracks = []
-  var lastAnnounce = null
   var groups = JSON.parse(fs.readFileSync('groups.json')) || []
 
   bot.on('message', (msg) => {
@@ -20,29 +19,17 @@ module.exports = (bot) => {
     fs.writeFileSync('groups.json', JSON.stringify(groups))
   })
 
-  function formatAnnounce () {
-    let message = ''
-    message += '🐻 ' + tracks[0].artist['#text'] + ' - ' + tracks[0].name + ' 🐻'
-    return message
-  }
-
-  function formatCommand () {
-    let message = ''
-    message += '🐻 ' + tracks[0].artist['#text'] + ' - ' + tracks[0].name + ' 🐻'
-    message += '\n' + moment(tracks[0].date).fromNow()
-    return message
-  }
-
-  function formatRecent (t) {
-    return '- [' + t.artist['#text'] + ' - ' + t.name + '](' + t.url + ') (' + moment(t.date).fromNow() + ')'
-  }
+  const formatTrack = t => `${t.artist['#text']} - ${t.name}`
+  const formatAnnounce = () => `🐻 ${formatTrack(tracks[0])} 🐻`
+  const formatCommand = () => `${formatAnnounce(tracks[0])}\n${moment(tracks[0].date).fromNow()}`
+  const formatRecent = t => `- [${t.artist['#text']} - ${t.name}](${t.url}) (${moment(t.date).fromNow()})`
 
   bot.onText(/\/np/, (msg) => {
     bot.sendMessage(msg.chat.id, formatCommand())
   })
 
   bot.onText(/\/recent/, (msg) => {
-    var tClone = tracks.slice(0).reverse()
+    const tClone = tracks.slice(0).reverse()
     bot.sendMessage(msg.chat.id, tClone.map(formatRecent).join('\n'), {
       'disable_web_page_preview': true,
       'parse_mode': 'markdown'
@@ -55,16 +42,8 @@ module.exports = (bot) => {
     }
   }
 
-  function trackHasBeenPlayedDuringTheLastHour (track) {
-    return ((new Date()) - track.date) <= ONE_HOUR
-  }
-
   function convertDate (track) {
-    if (track['@attr'] && track['@attr'].nowplaying === 'true') {
-      track.date = new Date()
-    } else {
-      track.date = new Date(parseInt(track.date.uts) * 1000)
-    }
+    track.date = new Date(parseInt(track.date.uts) * 1000)
     return track
   }
 
@@ -73,8 +52,7 @@ module.exports = (bot) => {
       return
     }
     tracks = res.track.map(convertDate)
-    if ((new Date() - track.date <= FETCH_INTERVAL) && (tracks[0].date - tracks[1].date > ONE_HOUR)) {
-      lastAnnounce = new Date()
+    if ((new Date() - tracks[0].date <= FETCH_INTERVAL) && (tracks[0].date - tracks[1].date > ONE_HOUR)) {
       announce()
     }
   }
@@ -83,11 +61,13 @@ module.exports = (bot) => {
     console.error(err)
   }
 
+  const startTimeout = () => setTimeout(fetch, FETCH_INTERVAL)
+
   function fetch () {
     lfm.user_getRecentTracks({
       user: 'matlu_klusteri',
       limit: 10
-    }).then(printRes, printError).then(() => setTimeout(fetch, FETCH_INTERVAL))
+    }).then(printRes, printError).then(startTimeout).catch(startTimeout)
   }
 
   fetch()
